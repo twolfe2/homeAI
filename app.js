4da5cb3628a1b7c90107dca9fc9a92968d398ecd
Sonam Dolker Kindy <sonam.d.kindy@gmail.com>
2016-07-16 11:56:11 -0700

    Video tag

--- app.js
+++ app.js
@@ -19,14 +19,14 @@ let mongoose = require('mongoose');
 let mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost/pizzadb';
 
 mongoose.connect(mongoUrl, err => {
-  console.log(err ||  `MongoDB connected at ${mongoUrl}`);
+	console.log(err || `MongoDB connected at ${mongoUrl}`);
 })
 
 
 let server = http.createServer(app);
 
 server.listen(PORT, err => {
-  console.log(err || `Server listening on port: ${PORT}`);
+	console.log(err || `Server listening on port: ${PORT}`);
 });
 
 // view engine setup
@@ -47,20 +47,20 @@ app.use('/api', require('./routes/api'));
 
 //////////////////////
 
-app.get('/', (req,res) => {
-  res.render('index', {title: 'Hello World!'});
+app.get('/', (req, res) => {
+	res.render('index', { title: 'NuLive' });
 });
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	let err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.send(err);
+	res.status(err.status || 500);
+	res.send(err);
 });
--- gulpfile.js
+++ gulpfile.js
@@ -11,22 +11,22 @@ const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 
 let paths = {
-  html: {
-    input: 'client/html/**/*.html',
-    output: 'public/html'
-  },
-  js: {
-    input: 'client/js/**/*.js',
-    output: 'public/js'
-  },
-  css: {
-    input: ['client/css/**/*.scss', 'client/css/**/*.sass'],
-    output: 'public/css'
-  },
-  favicon: {
-    input: './client/favicon.ico',
-    output: './public'
-  }
+	html: {
+		input: 'client/html/**/*.html',
+		output: 'public/html'
+	},
+	js: {
+		input: 'client/js/**/*.js',
+		output: 'public/js'
+	},
+	css: {
+		input: ['client/css/**/*.scss', 'client/css/**/*.sass'],
+		output: 'public/css'
+	},
+	favicon: {
+		input: './client/favicon.ico',
+		output: './public'
+	}
 };
 
 
@@ -37,80 +37,80 @@ let paths = {
 
 gulp.task('default', ['build', 'watch'], function() {
 
-  console.log('default');
+	console.log('default');
 
 });
 
 
 gulp.task('build', ['html', 'js', 'css', 'favicon']);
 
-gulp.task('watch', ['watch:html','watch:js', 'watch:css']);
+gulp.task('watch', ['watch:html', 'watch:js', 'watch:css']);
 
 
-gulp.task('favicon', function () {
-  return gulp.src(paths.favicon.input)
-    .pipe(gulp.dest(paths.favicon.output));
-})
-//////////////HTML////////////////////////////
+gulp.task('favicon', function() {
+		return gulp.src(paths.favicon.input)
+			.pipe(gulp.dest(paths.favicon.output));
+	})
+	//////////////HTML////////////////////////////
 
 
 gulp.task('html', ['clean:html'], function() {
-  return gulp.src(paths.html.input)
-    .pipe(plumber())
-    .pipe(gulp.dest(paths.html.output));
+	return gulp.src(paths.html.input)
+		.pipe(plumber())
+		.pipe(gulp.dest(paths.html.output));
 });
 
 gulp.task('clean:html', function() {
-  return del([paths.html.output]);
+	return del([paths.html.output]);
 });
 
 
 gulp.task('watch:html', function() {
-  gulp.watch(paths.html.input, ['html']);
+	gulp.watch(paths.html.input, ['html']);
 
 });
 
 //////////////JS////////////////////////////
 
 gulp.task('js', ['clean:js'], function() {
-  return gulp.src(paths.js.input)
-    .pipe(plumber())
-    .pipe(sourcemaps.init())    
-      .pipe(concat('bundle.js'))
-      .pipe(babel({
-        presets: ['es2015']
-      }))
-      .pipe(ngAnnotate())
-      .pipe(uglify())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(paths.js.output));
+	return gulp.src(paths.js.input)
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(concat('bundle.js'))
+		.pipe(babel({
+			presets: ['es2015']
+		}))
+		.pipe(ngAnnotate())
+		.pipe(uglify())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(paths.js.output));
 });
 
 gulp.task('clean:js', function() {
-  return del([paths.js.output]);
+	return del([paths.js.output]);
 });
 
 
 gulp.task('watch:js', function() {
-  gulp.watch(paths.js.input, ['js']);
+	gulp.watch(paths.js.input, ['js']);
 
 });
 
 ///////////////CSS/////////////////////////////////
 
 gulp.task('css', ['clean:css'], function() {
-  return gulp.src(paths.css.input)
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(gulp.dest(paths.css.output));
+	return gulp.src(paths.css.input)
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(gulp.dest(paths.css.output));
 });
 
 gulp.task('clean:css', function() {
-  return del([paths.css.output]);
+	return del([paths.css.output]);
 });
 
 
 gulp.task('watch:css', function() {
-  gulp.watch(paths.css.input, ['css']);
+	gulp.watch(paths.css.input, ['css']);
 
 });
